50b4aa99e04bc30085c3941425868864c0074f8f
fadhliyees <fadhliyees@gmail.com>
2024-12-24 14:10:53 +0700

    feat: slicing sign in and sign up

--- resources/js/Components/InputLabel.jsx
+++ resources/js/Components/InputLabel.jsx
@@ -1,17 +1,12 @@
 export default function InputLabel({
+    forInput,
     value,
     className = '',
     children,
     ...props
 }) {
     return (
-        <label
-            {...props}
-            className={
-                `block text-sm font-medium text-gray-700 ` +
-                className
-            }
-        >
+        <label {...props} className={`text-base block mb-2` + className}>
             {value ? value : children}
         </label>
     );
